6d30bc702c351845e2686f991e6421dfaf0a483c
Matteo Guarnerio <magic890@gmail.com>
2020-01-13 16:55:20 +0100

    GA tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-12872692-26',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
